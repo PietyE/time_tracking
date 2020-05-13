e4eb9cb8f71d90192b37ddf40256a2db2c63589e
Vovk Viktor <viktor.vovk@vilmate.com>
2020-05-13 22:37:30 +0300

    d

--- src/components/projects-report/ProjectsReport.js
+++ src/components/projects-report/ProjectsReport.js
@@ -47,7 +47,10 @@ function ProjectsReport({
   selectedProject = {},
 }) {
   useEffect(() => {
-    getDevelopersProjectInProjectReport()
+    if (roleUser !== DEVELOPER) {
+      getDevelopersProjectInProjectReport()
+    }
+
     getDeveloperConsolidateProjectReport()
   }, [])
 
