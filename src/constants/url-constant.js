f8540a23b034b441357a3e3b332be11a6a6421ae
Виктор Вовк <viktor.vovk@vilmate.com>
2020-06-16 15:23:57 +0000

    Updated url-constant.js

--- src/constants/url-constant.js
+++ src/constants/url-constant.js
@@ -1,5 +1,5 @@
-//export const BASE_URL = 'https://dev-api-timetracking.vilmate.com/api/v1'
+export const BASE_URL = 'https://dev-api-timetracking.vilmate.com/api/v1'
 
-export const BASE_URL = 'https://api-internal.vilmate.com'
+//export const BASE_URL = 'https://api-internal.vilmate.com'
 
 export const PAGE_SIZE = 50
