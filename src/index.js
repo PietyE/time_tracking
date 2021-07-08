55ea2497d60682ac8c44fa96ff47619b644b43ce
Zinaida Dubina <Zinaida.dubina@vilmate.com>
2021-07-08 10:38:50 +0300

    build(package, index): install @devexpress and its deps

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
+import '@devexpress/dx-react-grid-bootstrap4/dist/dx-react-grid-bootstrap4.css';
 import store from 'store'
 import App from 'components/App'
 
