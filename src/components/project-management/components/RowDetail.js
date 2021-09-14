4ceeffe081dbf3e2bc632e434e3c354684314051
Mykhailo Kostenko <mykhailo.kostenko@vilmate.com>
2021-09-14 07:55:56 +0300

    show the developer in the list if he has a 'hours', even if is_active = false

--- src/components/project-management/components/RowDetail.js
+++ src/components/project-management/components/RowDetail.js
@@ -32,7 +32,7 @@ const RowDetail = ({ row, currentProjectReport }) => {
 
   useEffect(() => {
     if (currentProjectReport) {
-      const activeProjectReports = currentProjectReport.users.filter(report => report.is_active === true)
+      const activeProjectReports = currentProjectReport.users.filter(report => report.is_active === true || report.hours)
       const reformatProjects = activeProjectReports.map(user => ({
         user: user.userName,
         occupancy: user.is_full_time ? 'Full-time' : 'Part-time',
