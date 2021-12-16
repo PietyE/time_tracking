979709a7abb9d0eca18467311f554906f6fa4515
Andrii Udot <andrii.udot@vilmate.com>
2021-12-16 18:25:32 +0200

    [TT-6230] Update TableHeader.js: - added SortButton component; - refactored styling of component;

--- src/components/projects-report/components/TableHeader.js
+++ src/components/projects-report/components/TableHeader.js
@@ -1,21 +1,33 @@
 import React from 'react'
 
 import { ADMIN, DEVELOPER, PM } from 'constants/role-constant'
+import SortButton from '../../project-management/components/SortButton'
 
-export default function TableHeader({ roleUser }) {
+export default function TableHeader({ roleUser, onSortPress }) {
   return (
     <div className="table_header_container">
       <div className="table_cell name">
         <span>Name</span>
+        <SortButton name="name" onSortPress={onSortPress}/>
       </div>
       <div className="table_cell project_name">
         <span>Projects</span>
+        <SortButton name="developer_projects" onSortPress={onSortPress}/>
       </div>
       {roleUser !== PM && (
         <>
-          <div className="table_cell salary">Salary</div>
-          <div className="table_cell rate">Rate</div>
-          <div className="table_cell hours">Hours</div>
+          <div className="table_cell salary">
+            Salary
+            <SortButton name="salary_uah" onSortPress={onSortPress}/>
+          </div>
+          <div className="table_cell rate">
+            Rate
+            <SortButton name="rate_uah" onSortPress={onSortPress}/>
+          </div>
+          <div className="table_cell hours">
+            Hours
+            <SortButton name="totalHoursOvertime" onSortPress={onSortPress}/>
+          </div>
           <div className="table_cell total">Overtime salary, total</div>
           <div className="table_cell total">Total salary</div>
           <div className="table_cell coast">Extra costs, UAH</div>
@@ -32,9 +44,8 @@ export default function TableHeader({ roleUser }) {
             {roleUser !== PM && (
                 <div className="table_cell ready">Payed</div>
             )}
-            </>
+        </>
       )}
-
     </div>
   )
 }
