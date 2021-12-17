f4b789846433ee0c702e1bf3b958aab4445de1d7
Andrii Udot <andrii.udot@vilmate.com>
2021-12-17 16:59:28 +0200

    [TT-6230] Update TableHeader.js: added currentSort state variable and updated styling of component;

--- src/components/projects-report/components/TableHeader.js
+++ src/components/projects-report/components/TableHeader.js
@@ -1,48 +1,126 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 
-import { ADMIN, DEVELOPER, PM } from 'constants/role-constant'
+import { DEVELOPER, PM } from 'constants/role-constant'
 import SortButton from '../../project-management/components/SortButton'
 
 export default function TableHeader({ roleUser, onSortPress }) {
+  const [currentSort, setCurrent] = useState(null);
+
+  const handleSortPress = useCallback((data) => {
+    onSortPress(data);
+    setCurrent(data?.key);
+  }, []);
+
   return (
     <div className="table_header_container">
       <div className="table_cell name">
         <span>Name</span>
-        <SortButton name="name" onSortPress={onSortPress}/>
+        <SortButton
+          name="name"
+          current={currentSort}
+          onSortPress={handleSortPress}
+        />
       </div>
       <div className="table_cell project_name">
         <span>Projects</span>
-        <SortButton name="developer_projects" onSortPress={onSortPress}/>
+        <SortButton
+          name="developer_projects"
+          current={currentSort}
+          onSortPress={handleSortPress}
+        />
       </div>
       {roleUser !== PM && (
         <>
           <div className="table_cell salary">
             Salary
-            <SortButton name="salary_uah" onSortPress={onSortPress}/>
+            <SortButton
+              name="salary_uah"
+              current={currentSort}
+              onSortPress={handleSortPress}
+            />
           </div>
           <div className="table_cell rate">
             Rate
-            <SortButton name="rate_uah" onSortPress={onSortPress}/>
+            <SortButton
+              name="rate_uah"
+              current={currentSort}
+              onSortPress={handleSortPress}
+            />
           </div>
           <div className="table_cell hours">
             Hours
-            <SortButton name="totalHoursOvertime" onSortPress={onSortPress}/>
+            <SortButton
+              name="totalHoursOvertime"
+              current={currentSort}
+              onSortPress={handleSortPress}
+            />
           </div>
-          <div className="table_cell total">Overtime salary, total</div>
-          <div className="table_cell total">Total salary</div>
-          <div className="table_cell coast">Extra costs, UAH</div>
-          <div className="table_cell to_pay">Total to pay, UAH</div>
+          <div className="table_cell total">
+            Overtime salary, total
+            <SortButton
+              name="total_overtimes"
+              current={currentSort}
+              onSortPress={handleSortPress}
+            />
+          </div>
+          <div className="table_cell total">
+            <span>
+              Total salary
+            </span>
+            <SortButton
+              name="total"
+              current={currentSort}
+              onSortPress={handleSortPress}
+            />
+          </div>
+          <div className="table_cell coast">
+            Extra costs, UAH
+            <SortButton
+              name="total_expenses"
+              current={currentSort}
+              onSortPress={handleSortPress}
+            />
+          </div>
+          <div className="table_cell to_pay">
+            Total to pay, UAH
+            <SortButton
+              name="total_uah"
+              current={currentSort}
+              onSortPress={handleSortPress}
+            />
+          </div>
         </>)}
       {roleUser === PM && (
         <>
-          <div className="table_cell hours">Hours</div>
+          <div className="table_cell hours">
+            Hours
+            <SortButton
+              name="totalHoursOvertime"
+              current={currentSort}
+              onSortPress={handleSortPress}
+            />
+          </div>
         </>
       )}
       {roleUser !== DEVELOPER && (
         <>
-          <div className="table_cell comment">Comments</div>
+          <div className="table_cell comment">
+            Comments
+            <SortButton
+              name="comments"
+              current={currentSort}
+              onSortPress={handleSortPress}
+            />
+          </div>
             {roleUser !== PM && (
-                <div className="table_cell ready">Payed</div>
+                <div className="table_cell ready">
+                  Payed
+                  <SortButton
+                    name="is_processed"
+                    current={currentSort}
+                    onSortPress={handleSortPress}
+                  />
+                </div>
             )}
         </>
       )}
