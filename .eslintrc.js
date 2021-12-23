dcd86a71d9824301f537b8b1003af006d82a4b13
Alexey Kurskoy <inprombank@gmail.com>
2021-12-23 18:44:24 +0200

    fix packages version

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,24 @@
 module.exports = {
   root: true,
-  parser: "@babel/eslint-parser",
+  parser: 'babel-eslint',
   env: {
     browser: true,
     jest: true,
   },
-  "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-        "ecmaFeatures": {
-            "jsx": true
-        }
+  ecmaFeatures: {
+    jsx: true,
   },
   plugins: ['react', 'react-hooks'],
   extends: ['eslint:recommended', 'plugin:react/recommended'],
   rules: {
-    "react/display-name": [0, { ignoreTranspilerName: 1 }],
+    'react/display-name': [0, { ignoreTranspilerName: 1 }],
     quotes: [1, 'single', { avoidEscape: true }],
-    "no-use-before-define": "off",
+    'no-use-before-define': 'off',
     // "react/jsx-filename-extension": "on",
-    // "react/prop-types": "off",
-    "comma-dangle": "off",
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
+    // "react/prop-types": "on",
+    'comma-dangle': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
   globals: {
     fetch: false,
