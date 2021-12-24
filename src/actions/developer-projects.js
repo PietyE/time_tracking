68352a554999b867a21c332178d0f1a8f46a8080
Andrii Udot <andrii.udot@vilmate.com>
2021-12-24 16:08:07 +0200

    Update developer-projects.js;

--- src/actions/developer-projects.js
+++ src/actions/developer-projects.js
@@ -1,7 +1,7 @@
 import {
   SET_DEVELOPER_PROJECTS,
   GET_DEVELOPER_PROJECTS,
-  GET_PROJECTS, SET_ERROR_RATES_LIST,
+  GET_PROJECTS,
 } from 'constants/actions-constant'
 
 export const getDeveloperProjects = (payload) => ({
