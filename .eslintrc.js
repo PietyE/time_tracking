aaa0c508144763d1be6ee003dda57817949a38a6
Andrii Udot <andrii.udot@vilmate.com>
2022-01-13 09:52:20 +0000

    Revert "Merged PR 3024: Make changes according to stage"

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,12 @@
 module.exports = {
   root: true,
-  parser: '@babel/eslint-parser',
+  parser: 'babel-eslint',
   env: {
     browser: true,
     jest: true,
+  },
+  ecmaFeatures: {
+    jsx: true,
   },
   plugins: ['react', 'react-hooks'],
   extends: ['eslint:recommended', 'plugin:react/recommended'],
@@ -11,13 +14,12 @@ module.exports = {
     'react/display-name': [0, { ignoreTranspilerName: 1 }],
     quotes: [1, 'single', { avoidEscape: true }],
     'no-use-before-define': 'off',
-    "react/jsx-filename-extension": "off",
-    'no-empty': 'off',
-    'no-unused-vars': 'off',
+    // "react/jsx-filename-extension": "on",
     "react/prop-types": "off",
     'comma-dangle': 'off',
     'react-hooks/rules-of-hooks': 'error',
-    // 'react-hooks/exhaustive-deps': 'on',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/prop-types': 'off',
   },
   globals: {
     fetch: false,
