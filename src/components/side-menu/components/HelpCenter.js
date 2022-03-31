85e9b707dc8d26ae3ebcf27d5e4a47a0e70c59dd
Serhij Zubrin <serhij.zubrin@vilmate.com>
2022-03-31 17:03:13 +0300

    Removed the page offset when scrolling

--- src/components/side-menu/components/HelpCenter.js
+++ src/components/side-menu/components/HelpCenter.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
 import { Link } from 'react-router-dom'
+import HintWindow from 'components/ui/HintWindow';
 
 
 const HelpCenter = (props) => {
-    const { img } = props;
+  const { img } = props;
 
+  const [showHint, setShowHint] = useState(false)
+
+  const sideMenuContainer = useRef()
+  const sideMenuWidth = sideMenuContainer.current?.clientWidth;
+  const WIDTH_SIDE_MENU = 78
+  
     return(
       <Link to="helpcenter" style={{ textDecoration: 'none' }}>
-        <div className="sidebar_menu_button">
-            <img src={img} alt="Help center" className="sidebar_img"/>
+        <div
+            ref={sideMenuContainer}
+            onMouseEnter={() => setShowHint(true)}
+            onMouseLeave={() => setShowHint(false)}
+            className="sidebar_menu_button">
+          <img src={img} alt="Help center" className="sidebar_img" />
+          {(showHint && sideMenuWidth === WIDTH_SIDE_MENU) && <HintWindow text={"Help center"} />}
             <span className="item">Help center</span>
         </div>
       </Link>
---
+++ src/components/ui/HintWindow/index.js
@@ -0,0 +1,3 @@
+import HintWindow from "./HintWindow";
+
+export default HintWindow
