fd6108f6a278e1be2d38776fb7da2a86e5ddabff
Serhij Zubrin <serhij.zubrin@vilmate.com>
2022-04-25 15:51:59 +0300

    Fixed eslint warnings in saga files

--- src/sagas/projectreport-saga.js
+++ src/sagas/projectreport-saga.js
@@ -1,10 +1,9 @@
-import { call, takeEvery, put, select, all } from 'redux-saga/effects'
+import { call, takeEvery, put, select } from 'redux-saga/effects'
 import Api from 'utils/api'
 import { pm } from '../api'
 import { showAler } from 'actions/alert'
 import { WARNING_ALERT, SUCCES_ALERT } from 'constants/alert-constant'
 import {
-  GET_DEV_CONSOLIDATE_PROJECT_REPORT,
   CHANGE_SELECTED_DATE_PROJECTS_REPORT,
   SET_SELECTED_DEVELOPER,
   CLEAR_SELECTED_DEVELOPER,
@@ -29,7 +28,7 @@ import {
   // setReportHistory
 } from 'actions/projects-report'
 import { getRatesList } from '../actions/currency'
-import { getSelectedMonthSelector, selectUsersId } from '../reducers/projects-report'
+import { getSelectedMonthSelector } from '../reducers/projects-report'
 import { consolidateReportMapper, usersProjectReportMapper } from '../utils/projectReportApiResponseMapper'
 import { selectActualCurrencyForUserList } from '../selectors/currency'
 import { getSelectedProjectIdSelector } from '../reducers/projects-management'
@@ -101,7 +100,7 @@ export function* getAllDevelopersProjectInProjectReport() {
 }
 
 function* setExchangeRate({ payload, callback }) {
-  const { month, year } = yield select(getSelectedMonthSelector)
+  // const { month, year } = yield select(getSelectedMonthSelector)
   try {
     const URL = 'exchange_rates/'
     const response = yield call([Api, 'saveExchangeRate'], URL, payload)
