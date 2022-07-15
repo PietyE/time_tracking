4e8483261b55c5020924d55a0f4323b71f6fed1c
Demian <demian@mai.ru>
2022-07-15 17:41:02 +0300

    edited link

--- src/components/common/linking.jsx
+++ src/components/common/linking.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
-const REGEXP_FOR_LINK = /^(https?:\/\/)?(www\.)?([a-zA-Z0-9]+(-?[a-zA-Z0-9])*\.)+[\w]{2,}(\/\S*)?$/gi
+const REGEXP_FOR_LINK =
+  /^(https?:\/\/)?(www\.)?([a-zA-Z0-9]+(-?[a-zA-Z0-9])*\.)+[\w]{2,}(\/\S*)?$/gi
 
 function Linking({ text }) {
   const result = text.split(/\s/)
   const arrString = []
   let index = 0
-  result.forEach(word => {
+  result.forEach((word) => {
     const test = REGEXP_FOR_LINK.test(word)
     if (arrString[index] === undefined) {
       arrString.push(word)
@@ -22,16 +23,18 @@ function Linking({ text }) {
 
   return (
     <>
-      {arrString.map(item => {
+      {arrString.map((item) => {
         if (item.search(REGEXP_FOR_LINK) !== -1) {
+          const link =
+            item.startsWith('http') || item.startsWith('https')
+              ? item
+              : `//${item}`
           return (
             <a
-              href={item}
+              href={link}
               target="_blank"
               rel="noopener noreferrer"
-              key={Math.random(2)
-                .toString(16)
-                .substring(2)}
+              key={Math.random(2).toString(16).substring(2)}
             >
               {item}{' '}
             </a>
