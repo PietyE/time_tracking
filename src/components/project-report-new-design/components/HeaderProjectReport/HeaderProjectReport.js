196cdda0e74a6aeff196e720aa95de54cd24cb91
Demian <demian@mai.ru>
2022-07-18 14:08:06 +0300

    added component google sync button

--- src/components/project-report-new-design/components/HeaderProjectReport/HeaderProjectReport.js
+++ src/components/project-report-new-design/components/HeaderProjectReport/HeaderProjectReport.js
@@ -1,16 +1,18 @@
-import React, { } from 'react'
+import React from 'react'
 import './headerProjectReport.scss'
+import { GoogleSyncButton } from './GoogleSyncButton'
 
-function HeaderProjectReport (props) {
-  const { name } = props;
+function HeaderProjectReport(props) {
+  const { name } = props
 
   return (
     <>
       <div className="project_report_header">
         <span className="header_title">{name}</span>
+        <GoogleSyncButton />
       </div>
     </>
   )
 }
 
-export default HeaderProjectReport;
+export default HeaderProjectReport
