fc3049a1c7e5caa395c77d3d04067dc6e1934966
Demian <demian@mai.ru>
2022-07-18 15:33:29 +0300

    added condition of render google drev sync button

--- src/components/project-report-new-design/components/HeaderProjectReport/HeaderProjectReport.js
+++ src/components/project-report-new-design/components/HeaderProjectReport/HeaderProjectReport.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import './headerProjectReport.scss'
 import { GoogleSyncButton } from './GoogleSyncButton'
+import { getRoleUser } from '../../../../selectors/user'
+import useShallowEqualSelector from '../../../../custom-hook/useShallowEqualSelector'
+import { ACCOUNTANT } from '../../../../constants/role-constant'
 
 function HeaderProjectReport(props) {
   const { name } = props
+
+  const role = useShallowEqualSelector(getRoleUser)
+
+  const renderGoogleDriveSyncButton = role === ACCOUNTANT && (
+    <GoogleSyncButton />
+  )
 
   return (
     <>
       <div className="project_report_header">
         <span className="header_title">{name}</span>
-        <GoogleSyncButton />
+        {renderGoogleDriveSyncButton}
       </div>
     </>
   )
