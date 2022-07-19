0511e89783b932c751a3371296bdd64d24b3dc23
Demian <demian@mai.ru>
2022-07-19 15:39:12 +0300

    added api request to get auth url

--- src/components/project-report-new-design/components/HeaderProjectReport/GoogleSyncButton/GoogleSyncButton.jsx
+++ src/components/project-report-new-design/components/HeaderProjectReport/GoogleSyncButton/GoogleSyncButton.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import { ReactComponent as GoogleDrive } from '../../../../../images/googleDriveButton/googleDrive.svg'
 import { CustomButton } from '../../../../ui/button'
+import { useDispatch } from 'react-redux'
 import './GoogleSyncButton.scss'
 
-export const GoogleSyncButton = () => (
-  <CustomButton
-    type="button"
-    startIcon={<GoogleDrive />}
-    className="header_google_drive_sync_button"
-    variant="outline-secondary"
-  >
-    Sync with Drive
-  </CustomButton>
-)
+export const GoogleSyncButton = () => {
+  const dispatch = useDispatch()
+
+  return (
+    <CustomButton
+      type="button"
+      startIcon={<GoogleDrive />}
+      className="header_google_drive_sync_button"
+      variant="outline-secondary"
+    >
+      Sync with Drive
+    </CustomButton>
+  )
+}
