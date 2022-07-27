f3c76924ff03cd69e424cdd03f7187db3a8b9f86
Kvasov Dmytro <dmytro.kvasov@vilmate.com>
2022-07-27 19:33:56 +0300

    added size to avatar component and added styles to button hover

--- src/components/vilmates-page/components/UsersList/components/UserItem/components/CardFooter/CardFooter.jsx
+++ src/components/vilmates-page/components/UsersList/components/UserItem/components/CardFooter/CardFooter.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { Button, CardActions } from '@material-ui/core'
+import './CardFooter.scss'
 
 export const CardFooter = () => (
   <CardActions>
-    <Button fullWidth color="primary" variant="outlined">
+    <Button
+      fullWidth
+      color="primary"
+      variant="outlined"
+      className="vilmate-card-footer-button"
+    >
       View Profile
     </Button>
   </CardActions>
