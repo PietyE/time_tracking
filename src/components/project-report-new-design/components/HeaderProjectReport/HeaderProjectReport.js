12eb54e5f685cf86316493dcf3f82741f6b233bf
Kvasov Dmytro <dmytro.kvasov@vilmate.com>
2022-07-29 16:59:05 +0300

    added visible button for both admin and accountant

--- src/components/project-report-new-design/components/HeaderProjectReport/HeaderProjectReport.js
+++ src/components/project-report-new-design/components/HeaderProjectReport/HeaderProjectReport.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import './headerProjectReport.scss'
 import { GoogleSyncButton } from './GoogleSyncButton'
-import { getRoleUser } from '../../../../selectors/user'
-import useShallowEqualSelector from '../../../../custom-hook/useShallowEqualSelector'
-import { ACCOUNTANT } from '../../../../constants/role-constant'
+import { getRoleUser } from 'selectors/user'
+import useShallowEqualSelector from 'custom-hook/useShallowEqualSelector'
+import { ACCOUNTANT, ADMIN } from 'constants/role-constant'
 
 function HeaderProjectReport(props) {
   const { name } = props
 
   const role = useShallowEqualSelector(getRoleUser)
 
-  const renderGoogleDriveSyncButton = role === ACCOUNTANT && (
-    <GoogleSyncButton />
-  )
+  const isHaveAccess = role === ADMIN || role === ACCOUNTANT
+
+  const renderGoogleDriveSyncButton = () => isHaveAccess && <GoogleSyncButton />
 
   return (
     <>
       <div className="project_report_header">
         <span className="header_title">{name}</span>
-        {renderGoogleDriveSyncButton}
+        {renderGoogleDriveSyncButton()}
       </div>
     </>
   )
