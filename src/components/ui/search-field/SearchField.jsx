82836f81c760cfa019eea65cf696f7d2fe8639f9
Kvasov Dmytro <dmytro.kvasov@vilmate.com>
2022-07-29 16:29:54 +0300

    added filtering

--- src/components/ui/search-field/SearchField.jsx
+++ src/components/ui/search-field/SearchField.jsx
@@ -3,15 +3,20 @@ import { TextField } from '@material-ui/core'
 import { ReactComponent as Search } from 'images/search-icon.svg'
 import './SearchField.scss'
 
-export const SearchField = ({ placeholder, endIcon }) => (
-  <TextField
-    type="search"
-    variant="outlined"
-    placeholder={placeholder}
-    className="search-field"
-    InputProps={{
-      startAdornment: <Search style={{ marginRight: '10px' }} />,
-      endAdornment: endIcon,
-    }}
-  />
-)
+export const SearchField = (props) => {
+  const { type, placeholder, endIcon, value, onChange } = props
+  return (
+    <TextField
+      type={type}
+      variant="outlined"
+      placeholder={placeholder}
+      value={value}
+      className="search-field"
+      InputProps={{
+        startAdornment: <Search style={{ marginRight: '10px' }} />,
+        endAdornment: endIcon,
+      }}
+      onChange={onChange}
+    />
+  )
+}
--- src/components/vilmates-page/components/UsersList/UsersList.jsx
+++ src/components/vilmates-page/components/UsersList/UsersList.jsx
@@ -1,13 +1,41 @@
-import React from 'react'
-import { Box, Grid, TextField, Typography } from '@material-ui/core'
+import React, { useCallback, useMemo, useState } from 'react'
+import { Box, Grid, Typography } from '@material-ui/core'
 import { users } from 'mocks/users'
 import { UserItem } from './components/UserItem'
 import { SearchField } from 'components/ui/search-field'
+// import { debounce } from 'lodash'
 import './UsersList.scss'
 
 export const UsersList = () => {
+  const [value, setValue] = useState('')
+  const [filteredUsers, setFilteredUsers] = useState(users)
+
+  const handleChange = (event) => setValue(event.target.value)
+
+  const searchingUsers = useMemo(
+    () =>
+      filteredUsers.filter(({ name }) =>
+        name.toLowerCase().trim().includes(value.toLowerCase().trim())
+      ),
+    [value]
+  )
+
+  const debounce = (func) => {
+    let timer
+    return function (...args) {
+      const context = this
+      if (timer) clearTimeout(timer)
+      timer = setTimeout(() => {
+        timer = null
+        func.apply(context, args)
+      }, 500)
+    }
+  }
+
+  const debounceChange = debounce(handleChange, 300)
+
   const renderUsers = () =>
-    users.map(({ id, name, position }) => (
+    searchingUsers.map(({ id, name, position }) => (
       <Grid item key={id} md={3}>
         <UserItem name={name} position={position} />
       </Grid>
@@ -16,7 +44,11 @@ export const UsersList = () => {
   return (
     <>
       <Box className="vilmate-action-var-container">
-        <SearchField placeholder="Search by name" />
+        <SearchField
+          placeholder="Search by name"
+          value={value}
+          onChange={debounceChange}
+        />
         <Typography>Reset filters</Typography>
       </Box>
       <Grid container spacing={7}>
