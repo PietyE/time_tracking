e79ada1cf73ab21d2f4abe38950d46e33ce8262f
Kvasov Dmytro <dmytro.kvasov@vilmate.com>
2022-08-02 20:13:07 +0300

    crated statisctis block with structure

---
+++ src/components/vilmates-page/components/Statistics/Statistics.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Box, Typography } from '@material-ui/core'
+import { ReactComponent as StatisticArrow } from 'images/vilmates/StatisticArrow.svg'
+import './Statistics.scss'
+
+export const Statistics = () => (
+  <Box className="vilmates-page-users-statistics-container">
+    <Box className="vilmates-page-users-statistics-amount-container">
+      <Box className="vilmates-page-users-statistics-amount-main-text-container">
+        <Typography variant="h6" component="p">
+          150
+        </Typography>
+        <Typography variant="body2" component="p">
+          Members
+        </Typography>
+      </Box>
+      <Box className="vilmates-page-users-statistics-amount-actions-container">
+        <Typography>+8</Typography>
+        <StatisticArrow />
+      </Box>
+    </Box>
+  </Box>
+)
