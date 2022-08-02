610257d232f2e1b8b8db56f566135b2d195cf63b
Kvasov Dmytro <dmytro.kvasov@vilmate.com>
2022-08-02 19:20:05 +0300

    added saga for getting all users on vilmate page

--- src/sagas/rootSaga.js
+++ src/sagas/rootSaga.js
@@ -6,11 +6,12 @@ import { watchErrorAlert } from './error-saga'
 import { watchTimereports } from './timereports-saga'
 import {
   watchDeveloperProjects,
-  // watchReportsV2 
+  // watchReportsV2
 } from './projectreport-saga'
 // import { watchCurrencies } from './currency-saga'
 
 import { watchProjectsManagement } from './projectsmanagement-saga'
+import { watchUsersListGetRequest } from './vilmates-page-saga'
 
 export function* rootSaga() {
   yield all([
@@ -21,6 +22,7 @@ export function* rootSaga() {
     watchDeveloperProjects(),
     // watchCurrencies(),
     watchProjectsManagement(),
+    watchUsersListGetRequest(),
     // watchReportsV2()
   ])
 }
---
+++ src/sagas/vilmates-page-saga.js
@@ -0,0 +1,35 @@
+import { put, takeEvery, call } from 'redux-saga/effects'
+import { VILMATES_PAGE_GET_USERS_LIST_REQUEST } from 'constants/vilmates-page'
+import {
+  vilmatesPageGetUsersListError,
+  vilmatesPageGetUsersListSuccess,
+} from 'actions/vilmates-page'
+import { showAlert } from 'actions/alert'
+import { WARNING_ALERT } from 'constants/alert-constant'
+import Api from 'api'
+
+function* getUsersList() {
+  try {
+    const url = 'users/'
+    const response = yield call([Api, 'users'], url)
+    const { status, data: users } = response
+    if (String(status)[0] !== '2') {
+      throw new Error()
+    }
+    yield put(vilmatesPageGetUsersListSuccess(users))
+  } catch (error) {
+    yield put(vilmatesPageGetUsersListError())
+    yield put(
+      showAlert({
+        type: WARNING_ALERT,
+        title: 'Something went wrong',
+        message: error.message || 'Something went wrong',
+        delay: 5000,
+      })
+    )
+  }
+}
+
+export function* watchUsersListGetRequest() {
+  yield takeEvery(VILMATES_PAGE_GET_USERS_LIST_REQUEST, getUsersList)
+}
