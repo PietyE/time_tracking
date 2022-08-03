50c001e23584c946bfd26084f783d188c4b5b3ca
Kvasov Dmytro <dmytro.kvasov@vilmate.com>
2022-08-03 18:21:01 +0300

    fixed conflicts

--- src/components/vilmates-page/components/UsersList/UsersList.jsx
+++ src/components/vilmates-page/components/UsersList/UsersList.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { Grid, Typography } from '@material-ui/core'
+import { Grid, Typography, Box } from '@material-ui/core'
 import { UserItem } from './components/UserItem'
 import useEqualSelector from 'custom-hook/useEqualSelector'
 import { getLoading, getUsers } from 'selectors/vilmates-page'
 import SpinnerStyled from 'components/ui/spinner'
-import { SearchField } from 'components/ui/search-field
+import { SearchField } from 'components/ui/search-field'
 import { useSearch } from 'custom-hook/useSearch'
 import { vilmatesPageGetUsersListRequest } from 'actions/vilmates-page'
 import './UsersList.scss'
+
 export const UsersList = () => {
   const dispatch = useDispatch()
   const users = useEqualSelector(getUsers)
@@ -23,8 +24,8 @@ export const UsersList = () => {
   }, [])
 
   //todo: do not forget to ask what to if we have not any user
-  const renderUsers = users.length ? (
-    users.map(({ id, name, position }) => (
+  const renderUsers = filteredUsers.length ? (
+    filteredUsers.map(({ id, name, position }) => (
       <Grid item key={id} md={3}>
         <UserItem name={name} position={position} />
       </Grid>
@@ -36,12 +37,6 @@ export const UsersList = () => {
   )
 
   return !isLoading ? (
-    <Grid container spacing={7}>
-      {renderUsers}
-    </Grid>
-  ) : (
-    <SpinnerStyled />
-  return (
     <>
       <Box className="vilmate-action-var-container">
         <SearchField
@@ -51,8 +46,10 @@ export const UsersList = () => {
         />
       </Box>
       <Grid container spacing={7}>
-        {renderUsers()}
+        {renderUsers}
       </Grid>
     </>
+  ) : (
+    <SpinnerStyled />
   )
 }
