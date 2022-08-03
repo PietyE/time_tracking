b50e0439ef247169644a9728c38da1a9a4d68f7d
Kvasov Dmytro <dmytro.kvasov@vilmate.com>
2022-08-03 20:24:49 +0300

    craeted saga logic for sync with google sheet

--- src/sagas/google-auth-success-saga.js
+++ src/sagas/google-auth-success-saga.js
@@ -2,14 +2,20 @@ import {
   GET_USERS_HOURS_TOKEN_REQUEST,
   GOOGLE_AUTH_SEND_GOOGLE_SHEET_SYNC_REQUEST,
 } from 'constants/google-auth-sucess-constants'
-import { takeEvery, put, call, takeLeading } from 'redux-saga/effects'
+import { takeEvery, put, call, takeLeading, select } from 'redux-saga/effects'
 import Api from 'utils/api'
 import {
   getUsersHoursTokenError,
   getUsersHoursTokenSuccess,
+  googleAuthSyncGoogleSheetError,
+  googleAuthSyncGoogleSheetSuccess,
 } from 'actions/google-auth-success'
 import { showAlert } from 'actions/alert'
 import { SUCCES_ALERT, WARNING_ALERT } from 'constants/alert-constant'
+import {
+  getGoogleSheetSyncInputLink,
+  googleSheetSyncIsAgree,
+} from 'selectors/google-auth-success'
 
 function* createUsersHoursToken(action) {
   try {
@@ -53,7 +59,41 @@ function* createUsersHoursToken(action) {
 }
 
 function* syncWithGoogleSheet() {
-  yield
+  try {
+    const url = 'user-hours/sync_with_google_sheets/'
+    const is_agree = yield select(googleSheetSyncIsAgree)
+    const spreadsheet = yield select(getGoogleSheetSyncInputLink)
+
+    const response = yield call([Api, 'syncWithGoogleSheet'], url, {
+      spreadsheet,
+      is_agree,
+    })
+
+    const { status } = response
+
+    if (String(status)[0] !== '2') {
+      throw new Error()
+    }
+
+    yield put(googleAuthSyncGoogleSheetSuccess())
+    yield put(
+      showAlert({
+        type: SUCCES_ALERT,
+        message: 'Authentication successfully',
+        delay: 4000,
+      })
+    )
+  } catch (error) {
+    yield put(googleAuthSyncGoogleSheetError())
+    yield put(
+      showAlert({
+        type: WARNING_ALERT,
+        title: 'Something went wrong',
+        message: error.message || 'Something went wrong',
+        delay: 5000,
+      })
+    )
+  }
 }
 
 export function* watchGoogleAuthCreateToken() {
