c551dd634469a10e1de5ae1cc833678149d87007
Kvasov Dmytro <dmytro.kvasov@vilmate.com>
2022-08-03 20:04:22 +0300

    modified reducer by google sheet sync actions

--- src/reducers/google-auth-sucess.js
+++ src/reducers/google-auth-sucess.js
@@ -2,6 +2,11 @@ import {
   GET_USERS_HOURS_TOKEN_ERROR,
   GET_USERS_HOURS_TOKEN_REQUEST,
   GET_USERS_HOURS_TOKEN_SUCCESS,
+  GOOGLE_AUTH_CHANGE_GOOGLE_SHEET_IS_AGREE,
+  GOOGLE_AUTH_CHANGE_GOOGLE_SHEET_LINK,
+  GOOGLE_AUTH_SEND_GOOGLE_SHEET_SYNC_REQUEST,
+  GOOGLE_AUTH_SYNC_GOOGLE_SHEET_ERROR,
+  GOOGLE_AUTH_SYNC_GOOGLE_SHEET_SUCCESS,
 } from 'constants/google-auth-sucess-constants'
 
 const initialState = {
@@ -23,6 +28,31 @@ export const googleAuthSuccess = (state = initialState, action) => {
         isLoading: false,
       }
     case GET_USERS_HOURS_TOKEN_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+      }
+    case GOOGLE_AUTH_CHANGE_GOOGLE_SHEET_LINK:
+      return {
+        ...state,
+        googleSheetLink: action.payload,
+      }
+    case GOOGLE_AUTH_CHANGE_GOOGLE_SHEET_IS_AGREE:
+      return {
+        ...state,
+        isAgree: !state.isAgree,
+      }
+    case GOOGLE_AUTH_SEND_GOOGLE_SHEET_SYNC_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+      }
+    case GOOGLE_AUTH_SYNC_GOOGLE_SHEET_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+      }
+    case GOOGLE_AUTH_SYNC_GOOGLE_SHEET_ERROR:
       return {
         ...state,
         isLoading: false,
