771f105eadf81ff240a782b93fd851226d1d67d4
Kvasov Dmytro <dmytro.kvasov@vilmate.com>
2022-08-09 18:54:17 +0300

    added template single page skeleton

---
+++ src/components/vilmate-sinngle-page/VilmateSinglePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { PageHeader } from '../vilmates-page/components/PageHeader'
+import { Container } from '../ui/container'
+
+export const VilmateSinglePage = () => {
+  return (
+    <Container>
+      <PageHeader name="Vilmates" />
+    </Container>
+  )
+}
