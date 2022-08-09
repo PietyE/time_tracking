aa8171a6d52e4be9c1154296b4196797bed6e70d
Kvasov Dmytro <dmytro.kvasov@vilmate.com>
2022-08-09 22:17:22 +0300

    add route to page

--- src/components/vilmate-sinngle-page/VilmateSinglePage.jsx
+++ src/components/vilmate-sinngle-page/VilmateSinglePage.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { PageHeader } from '../vilmates-page/components/PageHeader'
 import { Container } from '../ui/container'
+import { useParams } from 'react-router-dom/cjs/react-router-dom'
+import { useDispatch } from 'react-redux'
 
 export const VilmateSinglePage = () => {
+  const { userId } = useParams()
+  const dispatch = useDispatch()
+
   return (
     <Container>
       <PageHeader name="Vilmates" />
--- src/screens/VilmateSinglePageScreen.js
+++ src/screens/VilmateSinglePageScreen.js
@@ -1,3 +1,6 @@
+import React from 'react'
+import { VilmateSinglePage } from 'components/vilmate-sinngle-page/VilmateSinglePage'
+
 export default function VilmateSinglePageScreen() {
-  return
+  return <VilmateSinglePage />
 }
