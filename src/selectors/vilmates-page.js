cb1ec5efa2d2289da7bb2ba669a22f942206af8f
Kvasov Dmytro <dmytro.kvasov@vilmate.com>
2022-08-09 22:42:11 +0300

    added selected to get user

--- src/selectors/vilmates-page.js
+++ src/selectors/vilmates-page.js
@@ -1,2 +1,4 @@
 export const getUsers = (state) => state.vilmatesPage.users
 export const getLoading = (state) => state.vilmatesPage.isLoading
+export const getSelectedUser = (state) =>
+  state.vilmatesPage.singlePage.selectedUser
