f1aeb5c176344024e0ae3ed8e078be510236b198
Kvasov Dmytro <dmytro.kvasov@vilmate.com>
2022-08-09 22:37:49 +0300

    added saga for Single Page Template

--- src/sagas/vilmates-page-saga.js
+++ src/sagas/vilmates-page-saga.js
@@ -1,11 +1,15 @@
 import { put, takeEvery, call } from 'redux-saga/effects'
-import { VILMATES_PAGE_GET_USERS_LIST_REQUEST } from 'constants/vilmates-page'
+import {
+  VILMATES_PAGE_GET_USERS_LIST_REQUEST,
+  VILMATES_PAGE_SELECT_USER_REQUEST,
+} from 'constants/vilmates-page'
 import {
   vilmatesPageGetUsersListError,
   vilmatesPageGetUsersListSuccess,
+  vilmatesPageSelectUserSuccess,
 } from 'actions/vilmates-page'
 import { showAlert } from 'actions/alert'
-import { WARNING_ALERT } from 'constants/alert-constant'
+import { SUCCES_ALERT, WARNING_ALERT } from 'constants/alert-constant'
 import Api from 'utils/api'
 
 function* getUsersList() {
@@ -17,6 +21,42 @@ function* getUsersList() {
       throw new Error()
     }
     yield put(vilmatesPageGetUsersListSuccess(users))
+    yield put(
+      showAlert({
+        type: SUCCES_ALERT,
+        title: 'Users successfully loaded',
+        delay: 3000,
+      })
+    )
+  } catch (error) {
+    yield put(vilmatesPageGetUsersListError())
+    yield put(
+      showAlert({
+        type: WARNING_ALERT,
+        title: 'Something went wrong',
+        message: error.message || 'Something went wrong',
+        delay: 5000,
+      })
+    )
+  }
+}
+
+function* getSelectedUser(action) {
+  try {
+    const url = `users/${action.payload}`
+    const response = yield call([Api, 'users'], url)
+    const { status, data: user } = response
+    if (String(status)[0] !== '2') {
+      throw new Error()
+    }
+    yield put(vilmatesPageSelectUserSuccess(user))
+    yield put(
+      showAlert({
+        type: SUCCES_ALERT,
+        title: 'User successfully loaded',
+        delay: 3000,
+      })
+    )
   } catch (error) {
     yield put(vilmatesPageGetUsersListError())
     yield put(
@@ -32,4 +72,5 @@ function* getUsersList() {
 
 export function* watchUsersListGetRequest() {
   yield takeEvery(VILMATES_PAGE_GET_USERS_LIST_REQUEST, getUsersList)
+  yield takeEvery(VILMATES_PAGE_SELECT_USER_REQUEST, getSelectedUser)
 }
