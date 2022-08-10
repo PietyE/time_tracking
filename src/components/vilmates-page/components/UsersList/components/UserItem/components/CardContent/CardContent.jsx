b8a39de164c66038deffa03600c3af3839439670
Kvasov Dmytro <dmytro.kvasov@vilmate.com>
2022-08-10 11:07:37 +0300

    fixed color styles for text, and warning of invalid props of color

--- src/components/vilmates-page/components/UsersList/components/UserItem/components/CardContent/CardContent.jsx
+++ src/components/vilmates-page/components/UsersList/components/UserItem/components/CardContent/CardContent.jsx
@@ -1,25 +1,20 @@
 import React from 'react'
 import { CardContent as CardMain, Typography } from '@material-ui/core'
+import './CardContent.scss'
 
 export const CardContent = ({ name, position, email }) => (
   <CardMain className="vilmate-card-main-content">
     <Typography
-      color="secondary.contrastText"
       variant="body2"
       component="p"
       className="vilmate-card-position-title"
     >
       {name}
     </Typography>
-    <Typography
-      color="secondary.contrastText"
-      component="p"
-      className="vilmate-card-position-text"
-    >
+    <Typography component="p" className="vilmate-card-position-text">
       {position}
     </Typography>
     <Typography
-      color="secondary.contrastText"
       component="p"
       variant="body2"
       className="vilmate-card-position-email"
