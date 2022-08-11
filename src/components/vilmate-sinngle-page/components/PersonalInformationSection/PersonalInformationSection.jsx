b6c57a0d7adfc8a74b85ef9673a2f17855764d59
Kvasov Dmytro <dmytro.kvasov@vilmate.com>
2022-08-11 17:51:26 +0300

    create about section

--- src/components/vilmate-sinngle-page/components/PersonalInformationSection/PersonalInformationSection.jsx
+++ src/components/vilmate-sinngle-page/components/PersonalInformationSection/PersonalInformationSection.jsx
@@ -6,6 +6,9 @@ import {
   ListItem,
   ListItemText,
   ListItemAvatar,
+  Divider,
+  Typography,
+  Button,
 } from '@material-ui/core'
 
 import { ReactComponent as Email } from 'images/personalInfo/Email.svg'
@@ -14,7 +17,10 @@ import { ReactComponent as Slack } from 'images/personalInfo/Slack.svg'
 import { ReactComponent as ReportTo } from 'images/personalInfo/ReportTo.svg'
 import { ReactComponent as Phone } from 'images/personalInfo/Phone.svg'
 import { ReactComponent as Calendar } from 'images/personalInfo/calendar.svg'
+import { ReactComponent as ReadMore } from 'images/vilmates/backArrow.svg'
 import styles from './PersonalInformationSection.module.scss'
+
+const ReadMoreMemoized = React.memo(ReadMore)
 
 const personalInformationInfo = [
   {
@@ -59,11 +65,23 @@ export const PersonalInformationSection = () => {
     ))
   return (
     <RightSessionContainer title="Personal information">
-      <Box className="information">
-        <Box component="information_left">
-          <List className={styles.list}>{renderListItems()}</List>
-        </Box>
-        <Box className="information_right"></Box>
+      <Box className={styles.information}>
+        <List className={styles.list}>{renderListItems()}</List>
+        <Divider />
+      </Box>
+      <Box className={styles.about}>
+        <Typography variant="h5">About</Typography>
+        <Typography variant="body2" component="p">
+          Познакомьтесь с Димой, Python Developer из харьковской Project team.
+          Мы точно знаем, что Дима умеет отвечать на вопросы развёрнуто — из его
+          анкеты можно узнать про мечту знать и животные языки, кардинальный
+          разворот жизни всего за 1 год и восхищение женской красотой:)
+        </Typography>
+        <Button
+          endIcon={<ReadMoreMemoized style={{ transform: 'rotate(180deg)' }} />}
+        >
+          Read more
+        </Button>
       </Box>
     </RightSessionContainer>
   )
