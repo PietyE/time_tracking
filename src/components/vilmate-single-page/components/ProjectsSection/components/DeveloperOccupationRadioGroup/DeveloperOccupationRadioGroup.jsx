4474d11c45c9a5d3536a125ac530447348723156
--global <hlieb.shchehelskyi@vilmate.com>
2022-08-19 11:24:16 +0300

    Remove commented code

--- src/components/vilmate-single-page/components/ProjectsSection/components/DeveloperOccupationRadioGroup/DeveloperOccupationRadioGroup.jsx
+++ src/components/vilmate-single-page/components/ProjectsSection/components/DeveloperOccupationRadioGroup/DeveloperOccupationRadioGroup.jsx
@@ -1,42 +1,18 @@
 import { FormControlLabel, Radio, RadioGroup } from '@material-ui/core'
-import { vilmatesPageChangeUserOnProjectRequest } from 'actions/vilmates-page'
 import { OCCUPATION } from 'constants/vilmates-page'
-import React, { useMemo, useState } from 'react'
-import { useDispatch } from 'react-redux'
+import React, { useMemo } from 'react'
 import styles from './DeveloperOccupationRadioGroup.module.scss'
 
 const { PART_TIME, FULL_TIME } = OCCUPATION
 
 export const DeveloperOccupationRadioGroup = ({ onChange, isFullTime }) => {
-  // const dispatch = useDispatch()
-  // const [isFullTime, setIsFullTime] = useState(isFullTimeValue)
-  // const occupationValue = useMemo(
-  //   () => (isFullTime ? FULL_TIME : PART_TIME),
-  //   [isFullTime]
-  // )
-
-  // const changeDeveloperOccupation = (developerProjectId, isFullTime) => {
-  //   dispatch(
-  //     vilmatesPageChangeUserOnProjectRequest({
-  //       developerProjectId,
-  //       data: { is_full_time: isFullTime },
-  //     })
-  //   )
-  // }
-
   const occupationValue = useMemo(
-    () => (isFullTime === false ? PART_TIME : FULL_TIME
-      ),
+    () => (isFullTime === false ? PART_TIME : FULL_TIME),
     [isFullTime]
   )
 
-  // const handleOccupationTypeChange = () => {
-  //   changeDeveloperOccupation(developerProjectId, !isFullTime)
-  //   setIsFullTime((prevValue) => !prevValue)
-  // }
-
   const changeHandler = (e) => {
-    onChange(e.target.value === FULL_TIME);
+    onChange(e.target.value === FULL_TIME)
   }
 
   return (
