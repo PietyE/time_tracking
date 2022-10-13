2d008a17513bc11658ac00f495a1e21b97ebbf32
Kvasov Dmytro <dmytro.kvasov@vilmate.com>
2022-10-13 14:47:54 +0300

    changed validation for google sync

--- src/components/google-auth-success/components/GoogleSheetSyncButton/helpers/index.js
+++ src/components/google-auth-success/components/GoogleSheetSyncButton/helpers/index.js
@@ -1,5 +1,3 @@
-import { showAlert } from 'actions/alert'
-import { DANGER_ALERT } from 'constants/alert-constant'
 import {
   googleAuthIsAgreeFalse,
   googleAuthSendGoogleSheetSyncRequest,
@@ -7,19 +5,13 @@ import {
 
 const REGEXP_FOR_GOOGLE_SHEET = new RegExp('/spreadsheets/d/([a-zA-Z0-9-_]+)')
 
-export const onGoogleSync = (dispatch) => (googleSheetLink) => {
-  if (!REGEXP_FOR_GOOGLE_SHEET.exec(googleSheetLink)) {
-    dispatch(
-      showAlert({
-        type: DANGER_ALERT,
-        title: 'Something went wrong',
-        message:
-          'You paste invalid link to google sheet. Try sync again with correct link',
-        delay: 4000,
-      })
-    )
-  } else {
-    dispatch(googleAuthIsAgreeFalse())
-    dispatch(googleAuthSendGoogleSheetSyncRequest())
+export const onGoogleSync =
+  (dispatch) => (googleSheetLink, setValidationError) => {
+    if (!REGEXP_FOR_GOOGLE_SHEET.exec(googleSheetLink)) {
+      setValidationError(true)
+    } else {
+      setValidationError(false)
+      dispatch(googleAuthIsAgreeFalse())
+      dispatch(googleAuthSendGoogleSheetSyncRequest())
+    }
   }
-}
