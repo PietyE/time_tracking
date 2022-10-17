53cf7d4cfe73cf009b6adb963f616d2323bacdc9
Kvasov Dmytro <dmytro.kvasov@vilmate.com>
2022-10-17 13:21:23 +0300

    added new post comments validarion

--- src/components/vilmate-single-page/components/CommentsSection/components/AddComment/AddComment.jsx
+++ src/components/vilmate-single-page/components/CommentsSection/components/AddComment/AddComment.jsx
@@ -1,17 +1,16 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { useParams } from 'react-router-dom/cjs/react-router-dom'
-import { Box, TextField } from '@material-ui/core'
+import { Box, TextField, Typography } from '@material-ui/core'
 import { Avatar } from 'components/ui/avatar'
 import { vilmatesPagePostCommentsRequest } from 'actions/vilmates-page'
-import { showAlert } from 'actions/alert'
-import { WARNING_ALERT } from 'constants/alert-constant'
 import { ReactComponent as PostComment } from 'images/PostComment.svg'
 import styles from './AddComment.module.scss'
 
 export const AddComment = ({ name }) => {
   const { userId } = useParams()
   const [text, setText] = useState('')
+  const [validationError, setValidationError] = useState(false)
   const dispatch = useDispatch()
 
   const onChange = (event) => setText(event.target.value)
@@ -19,42 +18,44 @@ export const AddComment = ({ name }) => {
   const postComment = () => {
     if (text && text.length > 10 && text.length < 1000) {
       dispatch(vilmatesPagePostCommentsRequest({ userId, text }))
+      setValidationError(false)
       setText('')
-    } else
-      dispatch(
-        showAlert({
-          type: WARNING_ALERT,
-          title: 'Validation Error',
-          message: 'Comment should be more than 10 words and less than 1000',
-          delay: 4000,
-        })
-      )
+    } else setValidationError(true)
   }
 
+  const className = !validationError ? styles.comment : styles.commentError
+
   return (
-    <Box className={styles.container}>
-      <Avatar name="user" size="small" />
-      <TextField
-        type="text"
-        variant="outlined"
-        placeholder={`Add a comment about ${name}`}
-        className={styles.comment}
-        value={text}
-        onChange={onChange}
-        minRows="5"
-        multiline
-        fullWidth
-        InputProps={{
-          endAdornment: (
-            <Box
-              className={styles.commentButtonContainer}
-              onClick={postComment}
-            >
-              <PostComment />
-            </Box>
-          ),
-        }}
-      />
-    </Box>
+    <>
+      <Box className={styles.container}>
+        <Avatar name="user" size="small" />
+        <TextField
+          type="text"
+          variant="outlined"
+          placeholder={`Add a comment about ${name}`}
+          className={className}
+          value={text}
+          onChange={onChange}
+          minRows="5"
+          multiline
+          fullWidth
+          InputProps={{
+            endAdornment: (
+              <Box
+                className={styles.commentButtonContainer}
+                onClick={postComment}
+              >
+                <PostComment />
+              </Box>
+            ),
+          }}
+        />
+      </Box>
+      {validationError && (
+        <Typography color="error" align="center" className={styles.error}>
+          Comment should be more than 10 words and less than 1000
+        </Typography>
+      )}
+    </>
   )
 }
