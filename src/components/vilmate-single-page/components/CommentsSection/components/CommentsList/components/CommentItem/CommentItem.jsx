68f8a76f8b0dc853e7e91fe6ff23d31f0a5bf857
Kvasov Dmytro <dmytro.kvasov@vilmate.com>
2022-10-17 11:10:46 +0300

    added contants and id for comments

--- src/components/vilmate-single-page/components/CommentsSection/components/CommentsList/components/CommentItem/CommentItem.jsx
+++ src/components/vilmate-single-page/components/CommentsSection/components/CommentsList/components/CommentItem/CommentItem.jsx
@@ -4,7 +4,7 @@ import { Avatar } from 'components/ui/avatar'
 import { CommentItemContent } from './components/CommentItemContent'
 import styles from './CommentItem.module.scss'
 
-export const CommentItem = ({ name, text, date }) => (
+export const CommentItem = ({ name, text, date, id }) => (
   <Grid item xs={12} className={styles.comment_container}>
     <Avatar name={name} size="small" />
     <CommentItemContent text={text} date={date} name={name} />
--- src/components/vilmate-single-page/components/CommentsSection/components/CommentsList/components/CommentItem/components/CommentItemContent/CommentItemContent.jsx
+++ src/components/vilmate-single-page/components/CommentsSection/components/CommentsList/components/CommentItem/components/CommentItemContent/CommentItemContent.jsx
@@ -3,27 +3,29 @@ import moment from 'moment'
 import { Box, Paper, Typography } from '@material-ui/core'
 import styles from './CommentItemContent.module.scss'
 
-export const CommentItemContent = ({ text, date, name }) => (
-  <Box className={styles.content_container}>
-    <Paper className={styles.content}>
-      <Typography variant="body2" component="p" className={styles.name}>
-        {name}
-      </Typography>
-      <Typography
-        variant="body2"
-        component="p"
-        style={{ lineBreak: 'anywhere' }}
-      >
-        {text}
-      </Typography>
-    </Paper>
-    <Box className={styles.actions}>
-      <Typography variant="body2" component="p" className={styles.date}>
-        {moment(date).format('ll')}
-      </Typography>
-      <Typography variant="body2" color="error" className={styles.delete}>
-        Delete
-      </Typography>
+export const CommentItemContent = ({ text, date, name, id }) => {
+  return (
+    <Box className={styles.content_container}>
+      <Paper className={styles.content}>
+        <Typography variant="body2" component="p" className={styles.name}>
+          {name}
+        </Typography>
+        <Typography
+          variant="body2"
+          component="p"
+          style={{ lineBreak: 'anywhere' }}
+        >
+          {text}
+        </Typography>
+      </Paper>
+      <Box className={styles.actions}>
+        <Typography variant="body2" component="p" className={styles.date}>
+          {moment(date).format('ll')}
+        </Typography>
+        <Typography variant="body2" color="error" className={styles.delete}>
+          Delete
+        </Typography>
+      </Box>
     </Box>
-  </Box>
-)
+  )
+}
