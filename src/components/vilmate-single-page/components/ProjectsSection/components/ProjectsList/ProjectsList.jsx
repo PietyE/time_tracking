23d52a88aa9d7469af794c79220ef04d6c9dcd01
Kvasov Dmytro <dmytro.kvasov@vilmate.com>
2022-10-31 12:39:46 +0200

    Fixed deleteing of proejct

--- src/components/vilmate-single-page/components/ProjectsSection/components/ProjectsList/ProjectsList.jsx
+++ src/components/vilmate-single-page/components/ProjectsSection/components/ProjectsList/ProjectsList.jsx
@@ -10,23 +10,25 @@ export const ProjectsList = ({
   deleteProjectHandler,
   changeOccupationHandler,
 }) => {
-  const renderListItems = developerProjects.map((developerProject) => {
-    const { project, is_full_time: isFullTime } = developerProject
-    const ownerObject = findListItemById(users, project.owner)
-    const owner = ownerObject?.name || ownerObject?.email
+  const renderListItems = developerProjects
+    .filter((project) => project.is_active)
+    .map((developerProject) => {
+      const { project, is_full_time: isFullTime } = developerProject
+      const ownerObject = findListItemById(users, project.owner)
+      const owner = ownerObject?.name || ownerObject?.email
 
-    return (
-      <ProjectsListItem
-        key={project.id}
-        title={project.name}
-        isFullTimeValue={isFullTime}
-        owner={owner}
-        developerProjectId={developerProject.id}
-        onDelete={deleteProjectHandler}
-        onOccupationChange={changeOccupationHandler}
-      />
-    )
-  })
+      return (
+        <ProjectsListItem
+          key={project.id}
+          title={project.name}
+          isFullTimeValue={isFullTime}
+          owner={owner}
+          developerProjectId={developerProject.id}
+          onDelete={deleteProjectHandler}
+          onOccupationChange={changeOccupationHandler}
+        />
+      )
+    })
 
   return <List className={styles.list}>{renderListItems}</List>
 }
