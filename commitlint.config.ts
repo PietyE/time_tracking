9f2e96c99812bcd55515af09e0fe52d18cacc240
Kvasov Dmytro <dmytro.kvasov@vilmate.com>
2022-11-11 17:44:42 +0200

    feature(husky)!: configured commit type names for project

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,1 +1,20 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feature',
+        'styles',
+        'tests',
+        'refactoring',
+        'revert',
+        'fix',
+        'hotfix',
+        'docs',
+        'ci',
+      ],
+    ],
+  },
+};
