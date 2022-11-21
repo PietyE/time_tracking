1f2fcb39e9477ac3640f5100fbcd49d70ca63ca9
Kvasov Dmytro <dmytro.kvasov@vilmate.com>
2022-11-21 18:43:55 +0200

    feature(CONSTANTS): Created colors.

---
+++ src/constants/colors.ts
@@ -0,0 +1,37 @@
+export enum BlackColors {
+  MAIN = '#222222',
+  MAIN_OPACITY_45 = '#22222272',
+  MAIN_OPACITY_40 = '#22222266',
+  MAIN_OPACITY_30 = '#2222224c',
+}
+
+export enum GreyColors {
+  MAIN_TEXT = '#616161',
+  STROKE_FORM_OPACITY_20 = '#00000033',
+  STROKE_OPACITY_40 = '#C9C9C666',
+  DISABLED = '#ADADA7',
+  DISABLED_OPACITY_50 = '#adada77f',
+  DISABLED_FORM = '#FAFAFA',
+  LINE = '#EBEBEB',
+  MAIN = '#F3F5F5',
+}
+
+export enum GreenColors {
+  DARK = '#125856',
+  MAIN = '#009C98',
+  MAIN_OPACITY_50 = '#009C9880',
+  MAIN_OPACITY_10 = '#009C981A',
+  SECONDARY = '#99D7D6',
+  LIGHT = '#E7F1F0',
+  LIGHT_OPACITY_15 = '#576C6C26',
+}
+
+export enum OtherColors {
+  ERROR = '#F84242CC',
+  LINEAR_GRADIENT = '180deg, #E1A140 0%, #F84242 28.13%, #1864D6 65.1%, #C416C8 100%',
+}
+
+export enum BackgroundColors {
+  GREY = '#E5E5E1',
+  MILK = '#FAFAF6',
+}
