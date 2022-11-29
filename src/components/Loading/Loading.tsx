54282812af7deb5e344f04a8449c2735e322ee5c
Kvasov Dmytro <dmytro.kvasov@vilmate.com>
2022-11-29 15:10:25 +0200

    feature(MUI): Overwritten Loading component.

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,15 +1,23 @@
 import type { FC } from 'react';
-
+import { Stack } from '@mui/material';
 import Spinner from 'shared/components/Spinner';
+import { styles } from './styles';
 
 const Loading: FC = () => {
   return (
-    <div className='loading_container'>
+    <Stack
+      position='fixed'
+      flexDirection='row'
+      alignItems='center'
+      justifyContent='center'
+      bgcolor='background.grey'
+      sx={styles}
+    >
       <Spinner
         size={60}
         color='primary'
       />
-    </div>
+    </Stack>
   );
 };
 
---
+++ src/components/Loading/styles.ts
@@ -0,0 +1,6 @@
+import type { SxProps } from '@mui/material';
+
+export const styles: SxProps = {
+  width: '100%',
+  height: '100vh',
+};
