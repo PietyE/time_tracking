91aecf56edd6239cadd92b4877b47f6529c4e607
Kvasov Dmytro <dmytro.kvasov@vilmate.com>
2022-12-08 21:45:59 +0200

    refactoring(MAINMENU): Refactored isActive nav link.

--- src/components/MainMenu/components/MainMenuPanels/components/MainMenuPanelsItem/components/MainMenuPanelsItemButton/MainMenuPanelsItemButton.tsx
+++ src/components/MainMenu/components/MainMenuPanels/components/MainMenuPanelsItem/components/MainMenuPanelsItemButton/MainMenuPanelsItemButton.tsx
@@ -1,6 +1,7 @@
 import { type FC } from 'react';
 import { Grid, Box, Typography } from '@mui/material';
 import { NavLink } from 'react-router-dom';
+import type { classNameWithIsActiveNavLink } from 'shared/types';
 import type { PanelButton } from 'components/MainMenu/components/MainMenuPanels/types';
 import { styles } from './styles';
 
@@ -10,27 +11,30 @@ interface Props {
 
 export const MainMenuPanelsItemButton: FC<Props> = ({
   button,
-}): JSX.Element => (
-  <NavLink
-    to={button.pathname}
-    className={({ isActive }) =>
-      isActive ? 'main_menu_link active' : 'main_menu_link'
-    }
-  >
-    <Box
-      sx={styles.buttonContainer}
-      position='relative'
+}): JSX.Element => {
+  const className: classNameWithIsActiveNavLink = ({ isActive }) =>
+    isActive ? 'main_menu_link active' : 'main_menu_link';
+
+  return (
+    <NavLink
+      to={button.pathname}
+      className={className}
     >
-      <Grid
-        container
-        alignItems='center'
-        sx={styles.buttonContentContainer}
+      <Box
+        sx={styles.buttonContainer}
+        position='relative'
       >
-        <Grid item>{button.icon}</Grid>
-        <Grid item>
-          <Typography>{button.label}</Typography>
+        <Grid
+          container
+          alignItems='center'
+          sx={styles.buttonContentContainer}
+        >
+          <Grid item>{button.icon}</Grid>
+          <Grid item>
+            <Typography>{button.label}</Typography>
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
-  </NavLink>
-);
+      </Box>
+    </NavLink>
+  );
+};
--- src/shared/types/index.tsx
+++ src/shared/types/index.tsx
@@ -1,1 +1,7 @@
 export type ReverseMap<T> = T[keyof T];
+
+export type classNameWithIsActiveNavLink = ({
+  isActive,
+}: {
+  isActive: boolean;
+}) => string;
