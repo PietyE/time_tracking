06022b5409221fdabfb03384c1728537b62a146f
Kvasov Dmytro <dmytro.kvasov@vilmate.com>
2022-12-08 02:03:07 +0200

    fix(ARCHITECTURE): Updated architecture structure of project.

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,36 @@
+import { type FC } from 'react';
+import { Stack } from '@mui/material';
+import { Navigate } from 'react-router-dom';
+import SignInForm from './components/SignInForm';
+import { AppRoutes } from 'constants/appRoutesConstants';
+import { useAppSelector } from 'hooks/redux';
+import { getIsAuthProfileSelector } from 'redux/slices/profile';
+import LogoSvg from 'shared/svg/Logo';
+import { styles } from './styles';
+
+export const Login: FC = (): JSX.Element => {
+  const isAuth = useAppSelector(getIsAuthProfileSelector);
+
+  if (isAuth) {
+    return (
+      <Navigate
+        to={AppRoutes.root}
+        replace
+      />
+    );
+  }
+
+  return (
+    <Stack
+      component='section'
+      className='login-page'
+      width='auto'
+      height='100vh'
+      alignItems='center'
+      sx={styles.container}
+    >
+      <LogoSvg />
+      <SignInForm />
+    </Stack>
+  );
+};
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,40 +1,6 @@
 import type { FC } from 'react';
-import { Stack } from '@mui/material';
-import { Navigate } from 'react-router-dom';
+import { Login } from 'components/Login';
 
-import SignInForm from './components/SignInForm';
-import { getIsAuthProfileSelector } from 'redux/slices/profile';
-import { useAppSelector } from 'hooks/redux';
-
-import LogoSvg from 'shared/svg/Logo';
-import { AppRoutes } from 'constants/appRoutesConstants';
-import { styles } from './styles';
-
-const LoginPage: FC = () => {
-  const isAuth = useAppSelector(getIsAuthProfileSelector);
-
-  if (isAuth) {
-    return (
-      <Navigate
-        to={AppRoutes.root}
-        replace
-      />
-    );
-  }
-
-  return (
-    <Stack
-      component='section'
-      className='login-page'
-      width='auto'
-      height='100vh'
-      alignItems='center'
-      sx={styles.container}
-    >
-      <LogoSvg />
-      <SignInForm />
-    </Stack>
-  );
-};
+const LoginPage: FC = () => <Login />;
 
 export default LoginPage;
