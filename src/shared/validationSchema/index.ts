a1085fdab22c5dda7d3f352a2316aeacfd7895ef
Kvasov Dmytro <dmytro.kvasov@vilmate.com>
2023-01-26 00:28:33 +0200

    fix(WORKITEMS): Added one more case for validation and error name fix.

--- src/shared/validationSchema/index.ts
+++ src/shared/validationSchema/index.ts
@@ -15,9 +15,9 @@ export const workItemValidationSchema = Yup.object().shape({
   reportText: Yup.string()
     .min(3, minSymbols('Report', '3'))
     .max(1000, maxSymbols('Report', '1000')),
-  time: Yup.string().test('validTime', 'Not valid', (value) => {
+  time: Yup.string().test('validTime', 'Invalid', (value) => {
     const [hour, min]: string[] = value?.split(':') ?? ['0:00'];
     const takeTime = hour ? +hour * 60 + +min : +min;
-    return !(!value || value === '0:00' || takeTime > 480);
+    return !(!value || value === '0:00' || takeTime > 480 || Number(min) > 59);
   }),
 });
