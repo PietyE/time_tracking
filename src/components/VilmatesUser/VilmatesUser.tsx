cadc94b2f4380c302bed63e861445da86f087d75
Kvasov Dmytro <dmytro.kvasov@vilmate.com>
2023-02-14 00:13:05 +0200

    feature(VILMATES): Started Vilmates Page.

--- src/components/VilmatesUser/VilmatesUser.tsx
+++ src/components/VilmatesUser/VilmatesUser.tsx
@@ -1,10 +1,12 @@
 import { type FC } from 'react';
-import { VilmateUserGoBackButton } from './components/VilmateUserGoBackButton';
+import { VilmatesUserGoBackButton } from './components/VilmatesUserGoBackButton';
+import { VilmatesUserInformation } from './components/VilmatesUserInformation';
 import { MemoizedPageHeader } from 'shared/components/PageHeader';
 
 export const VilmatesUser: FC = (): JSX.Element => (
   <>
     <MemoizedPageHeader title='Vilmates' />
-    <VilmateUserGoBackButton />
+    <VilmatesUserGoBackButton />
+    <VilmatesUserInformation />
   </>
 );
