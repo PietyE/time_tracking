bac936a1f8bf6c64549a72401fe1ced921535c88
Kvasov Dmytro <dmytro.kvasov@vilmate.com>
2023-02-21 01:17:45 +0200

    feature(PM): Added filter to PM.

--- src/components/ProjectManagement/components/ProjectManagementFilter/ProjectManagementFilter.tsx
+++ src/components/ProjectManagement/components/ProjectManagementFilter/ProjectManagementFilter.tsx
@@ -1,15 +1,43 @@
-import { type FC } from 'react';
+import { type FC, useEffect } from 'react';
 import { Box, Grid } from '@mui/material';
-// import { Autocomplete } from 'shared/components/Autocomplete';
+import { getProjects } from 'redux/asyncActions/projects';
+import { getUsers } from 'redux/asyncActions/users';
+import { selectDeveloper, selectProject } from 'redux/slices/projectReport';
 import { SelectMonthMemoized } from 'shared/components/SelectMonth';
-import { useAppShallowSelector } from 'hooks/redux';
-// import { getUsers } from 'redux/asyncActions/users';
-import { getUsersLoading } from 'redux/selectors/users';
+import { useAppDispatch, useAppShallowSelector } from 'hooks/redux';
+import {
+  getUsers as getUsersSelector,
+  getUsersLoading,
+} from 'redux/selectors/users';
 import { SkeletonWrapper } from 'shared/components/SkeletonWrapper';
+import {
+  getIsProjectsLoading,
+  getProjects as getProjectsSelector,
+} from 'redux/selectors/projects';
+import { Autocomplete } from 'shared/components/Autocomplete';
+import type { User } from 'api/models/users';
+import type { Project } from 'api/models/projects';
 
 export const ProjectManagementFilter: FC = (): JSX.Element => {
-  // const users = useAppShallowSelector(getUsers);
+  const users = useAppShallowSelector(getUsersSelector);
   const isUsersLoading = useAppShallowSelector(getUsersLoading);
+  const projects = useAppShallowSelector(getProjectsSelector);
+  const isProjectsLoading = useAppShallowSelector(getIsProjectsLoading);
+  const dispatch = useAppDispatch();
+
+  const changeUser = (user: object): void => {
+    dispatch(selectDeveloper(user as Omit<User, 'permissions'>));
+  };
+
+  const changeDeveloperProject = (developerProject: object): void => {
+    dispatch(selectProject(developerProject as Project));
+  };
+
+  useEffect(() => {
+    if (!projects?.length) void dispatch(getProjects());
+    if (!users?.length) void dispatch(getUsers());
+  }, [dispatch]);
+
   return (
     <Grid
       container
@@ -20,25 +48,25 @@ export const ProjectManagementFilter: FC = (): JSX.Element => {
         <Grid container>
           <Grid item>
             <SkeletonWrapper isLoading={isUsersLoading}>
-              {/* {!!users.length && ( */}
-              {/*  <Autocomplete */}
-              {/*    options={[{}]} */}
-              {/*    keysToName={} */}
-              {/*    keysToId={} */}
-              {/*    onChange={} */}
-              {/*  /> */}
-              {/* )} */}
+              {!!users.length && (
+                <Autocomplete
+                  options={users}
+                  keysToName={['users']}
+                  keysToId={['id']}
+                  onChange={changeUser}
+                />
+              )}
             </SkeletonWrapper>
           </Grid>
-          <SkeletonWrapper isLoading={[{}]}>
-            {/* <Grid item> */}
-            {/*  <Autocomplete */}
-            {/*    options={} */}
-            {/*    keysToName={} */}
-            {/*    keysToId={} */}
-            {/*    onChange={} */}
-            {/*  /> */}
-            {/* </Grid> */}
+          <SkeletonWrapper isLoading={isProjectsLoading}>
+            <Grid item>
+              <Autocomplete
+                options={projects}
+                keysToName={['name']}
+                keysToId={['id']}
+                onChange={changeDeveloperProject}
+              />
+            </Grid>
           </SkeletonWrapper>
         </Grid>
       </Grid>
--- src/redux/slices/projectManagements.ts
+++ src/redux/slices/projectManagements.ts
@@ -1,17 +1,34 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+import type { Project } from 'api/models/projects';
+import type { User } from 'api/models/users';
 
 interface InitialState {
   isLoading: boolean;
+  selectedProject: Project;
+
+  selectedDeveloper: Omit<User, 'permissions'>;
 }
 
 const initialState: InitialState = {
   isLoading: true,
+  selectedProject: {} as Project,
+  selectedDeveloper: {} as Omit<User, 'permissions'>,
 };
 
 const projectManagements = createSlice({
   name: 'projectManagements',
   initialState,
-  reducers: {},
+  reducers: {
+    selectProject: (state, action: PayloadAction<Project>) => {
+      state.selectedProject = action.payload;
+    },
+    selectDeveloper: (
+      state,
+      action: PayloadAction<Omit<User, 'permissions'>>,
+    ) => {
+      state.selectedDeveloper = action.payload;
+    },
+  },
 });
 
 export default projectManagements.reducer;
